62d28514a898a51fccfda9150b6da97fead26437
Berend <berend@inuits.eu>
2023-01-17 11:25:31 +0100

    Simplify resolver

--- advancedFilterResolver.ts
+++ advancedFilterResolver.ts
@@ -1,17 +1,14 @@
-import {
-  resolveEntityFilters,
-  resolveMediafileFilters,
-} from '../../resolvers/filterResolver';
-import { Resolvers } from '../../type-defs';
+import { resolveFilters } from '../../resolvers/filterResolver';
+import { Collection, Resolvers } from '../../type-defs';
 import { ContextValue } from '../../types';
 
 export const advancedFilterResolver: Resolvers<ContextValue> = {
   Query: {
     advancedFilters: async (_source, { choice }, { dataSources }) => {
       if (choice === 'mediaFileFilters') {
-        return await resolveMediafileFilters(dataSources);
+        return await resolveFilters(dataSources, Collection.Mediafiles);
       } else {
-        return await resolveEntityFilters(dataSources);
+        return await resolveFilters(dataSources, Collection.Entities);
       }
     },
   },
