0198eceed9013c30005857d174f9426d4b107941
Berend Vandenbussche <berend@inuits.eu>
2023-02-16 09:24:11 +0100

    Export filterinputparser

--- advancedFilterModule.ts
+++ advancedFilterModule.ts
@@ -1,12 +1,18 @@
-import { createModule } from 'graphql-modules';
-import { advancedFilterSchema } from './advancedFilterSchema.schema';
-import { advancedFilterResolver } from './advancedFilterResolver';
+import { createModule } from "graphql-modules";
+import { advancedFilterSchema } from "./advancedFilterSchema.schema";
+import { advancedFilterResolver } from "./advancedFilterResolver";
+import { filterInputParser } from "./parsers/advancedFilterParser";
 
 const advancedFilterModule = createModule({
-  id: 'advancedFilterModule',
+  id: "advancedFilterModule",
   dirname: __dirname,
   typeDefs: [advancedFilterSchema],
   resolvers: [advancedFilterResolver],
 });
 
-export { advancedFilterModule, advancedFilterResolver, advancedFilterSchema };
+export {
+  advancedFilterModule,
+  advancedFilterResolver,
+  advancedFilterSchema,
+  filterInputParser,
+};
