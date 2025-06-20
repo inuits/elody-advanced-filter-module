fc1c23c7f4060b2d392678a50375f7746681b982
Jonas Verschueren <jonasv@inuits.eu>
2025-06-20 14:01:36 +0000

    Revert "fix: Add geo filter type"
    This reverts commit 79c4fe91afab532a612cab878bace53028a06702

--- advancedFilterSchema.schema.ts
+++ advancedFilterSchema.schema.ts
@@ -146,7 +146,6 @@ export const advancedFilterSchema = gql`
     boolean: [String!]!
     type: [String!]!
     metadata_on_relation: [String!]!
-    geo: [String!]!
   }
 
   type LookupInputType {
